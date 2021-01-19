c4917706086e315ec0cc410063a7fe8a122b1f0b
Kirill Gavrilchik <gavrilchikkirill6@gmail.com>
2021-01-19 13:41:41 +0300

    feat: refactor contoller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,4 @@
 const usersService = require('../services/users.service');
-const User = require('../models/User.js');
 class UsersController {
 
     service = usersService;
@@ -11,43 +10,21 @@ class UsersController {
     }
 
     delete = async (req, res) => {
-        const candidate = await User.findOne({ login: req.body.login})
-        if (candidate) {
             res
             .status(200)
             .send(await this.service.deleteUser(req.body.login))
-        } else {
-            res.status(404).json({
-                message: 'Пользователь с таким логином не найден.'
-            })
-        }
     }
 
     login = async (req, res) => {
-        const candidate = await User.findOne({ login: req.body.login})
-        if(candidate) {
             res 
             .status(200)
             .send(await this.service.login(req.body.login, req.body.password))
-        } else {
-            res.status(404).json({
-                message: 'Пользователь с таким логином не найден.'
-            })
-        }
     }
 
     register = async (req, res) => {
-        const candidate = await User.findOne({ login: req.body.login})
-        if(candidate) {
-            res.status(409).json({
-                message: "Такой логин уже занят. Попробуйте другой"
-            })
-
-        } else {
             res 
             .status(201)
             .json(await this.service.register(req.body.login, req.body.password))
-        }
     }
 }
 
--- services/users.service.js
+++ services/users.service.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys.js');
 const User = require('../models/User.js');
@@ -10,30 +11,47 @@ class UsersService {
     }
 
     deleteUser = async (login) => {
-         await User.remove({ login: login})
+        const candidate = await User.findOne({ login: login})
+        if (candidate) {
+            await User.remove({ login: login})
+            return candidate;
+        } else {
+            return createError(404, 'Пользователь с таким логином не найден.');
+        }
+         
     }
 
     login = async (login, password) => {
         const candidate = await User.findOne({ login: login})
-        const passwordResult = await bcrypt.compare(password, candidate.password);
-        if (passwordResult) {
-            const token = jwt.sign({
-                login: candidate.login,
-                userId: candidate._id
-            }, keys.jwt, {expiresIn: 60 * 60})
-            return {token, candidate};
+        if(candidate) {
+            const passwordResult = await bcrypt.compare(password, candidate.password);
+            if (passwordResult) {
+                const token = jwt.sign({
+                    login: candidate.login,
+                    userId: candidate._id
+                }, keys.jwt, {expiresIn: 60 * 60})
+                return {token, candidate};
+            } else {
+                return createError(404, 'Пароль введен неверно.');
+            }
+        } else {
+            return createError(404, 'Пользователь с таким логином не найден.');
         }
-
     }
 
     register = async (login, password) => {
-        const salt = await bcrypt.genSalt(10);
+        const candidate = await User.findOne({ login: login})
+        if(candidate) {
+            return createError(409, 'Такой логин уже занят. Попробуйте другой.');
+        } else {
+            const salt = await bcrypt.genSalt(10);
         const user = new User({
             login: login,
             password:await bcrypt.hash(password, salt)
         })
         await user.save();
         return user;
+        }
     }
 }
 
